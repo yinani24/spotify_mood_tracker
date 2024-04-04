96617720582dbfd4d5fd2f359441207f7545cd57
Yash <yinani@ucdavis.edu>
2024-04-03 22:18:13 -0700

    new highs

--- mood_tracker/app/callback/page.tsx
+++ mood_tracker/app/callback/page.tsx
@@ -1,36 +1,38 @@
 'use client'
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
-import Dashboard from '../dashboard/page';
-// import Link from 'next/link';
-// import {cookies} from 'next/headers';
+//import Dashboard from '../dashboard/page';
 
-export default function Callback(){
-    const searchParams = useSearchParams()
-    const code = searchParams.get('code')
-    const api = `api?code=${code}`
-    const router = useRouter()
+export default function Callback() {
+    const searchParams = useSearchParams();
+    const code = searchParams.get('code');
+    const api = `api?code=${code}`;
+    const router = useRouter();
+    const [fetched, setFetched] = useState(false); // State to track whether API has been fetched
+
     const fetchAPI = async () => {
-        // const cookie = cookieStore.get('verifier')
-        // console.log(cookie)
-        // const code = searchParams.get('code')            
-        const profile = await fetch(`${api}`)
-        if(profile.status === 200){
-            router.push('/dashboard')
+        // Prevent fetching API multiple times
+        if (!fetched) {
+            const profile = await fetch(api);
+            console.log("Profile: ", profile)
+            setFetched(true); // Set fetched to true after fetching once
+            if (profile.status === 200) {
+                router.push('/dashboard');
+            } else {
+                router.push('/');
+            }
         }
-        else{
-            router.push('/')
-        }
-    }
+    };
+
     useEffect(() => {
-        fetchAPI()
-    },[])
+        console.log('Fetching API...');
+        fetchAPI();
+    }, []); // Execute only on component mount
 
     return (
         <section className='flex justify-center items-center w-full min-h-screen'>
             <h1 className='text-2xl text-[#FFFFFF] font-[Inter] font-bold'>Logging You In...</h1>
-            {/* <Link href={`${api}`}></Link> */}
         </section>
-    )
+    );
 }
